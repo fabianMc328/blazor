8e2b60e5ccffe0c30c50caaa41621ffa27b351cf
fabian <blackccks020@gmail.com>
2025-10-28 21:27:42 -0700

    Agregar archivos de proyecto.

---
+++ Components/Data/Juego.cs
@@ -0,0 +1,11 @@
+﻿namespace blazor.Components.Data
+{
+    public class Juego
+    {
+        public int Identificador { get; set; }
+        public string Nombre { get; set; }
+        public bool Jugado { get; set; }
+
+
+    }
+}
---
+++ Components/Data/ServicioJuegos.cs
@@ -0,0 +1,19 @@
+﻿namespace blazor.Components.Data
+{
+    public class ServicioJuegos
+    {
+
+        private List<Juego> juegos = new List<Juego>
+        {
+            new Juego{Identificador=1,Nombre="Ravel",Jugado=false},
+            new Juego{Identificador=2,Nombre="Carcassonne",Jugado=true}
+        };
+        public Task<List<Juego>> ObtenerJuegos() => Task.FromResult(juegos);
+        public Task AgregarJuego(Juego juego)
+        {
+            juegos.Add(juego);
+            return Task.CompletedTask;
+        }
+      
+    }
+}
